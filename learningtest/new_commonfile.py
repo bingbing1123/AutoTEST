bbe2774ddc47f5d43e4108fae29249eae08efddc
jiyanjiao <771469575@qq.com>
2018-11-28 14:34:45 +0800

    提交

---
+++ learningtest/new_commonfile.py
@@ -0,0 +1,10 @@
+import os
+path=os.path.abspath(".")
+print("new_commonfile.py文件中path=os.path.abspath(.)获得的路径为==",path)
+gFirstPicturePath = ""
+gPicturePath = path+"\\1.png"
+gOnePicturePath =path+"\\1_2.png"
+gOneResult = []
+gAllResult = []
+print("new_commonfile.py文件中的gAllResult = []的值为=====",gAllResult)
+gAllFileName = ""
